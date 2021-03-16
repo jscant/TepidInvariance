f51e5c1eaa123eaa54d8c727e194617d82aa2162
Jack Scantlebury <jack.scantlebury@gmail.com>
2021-03-16 11:14:37 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='tepid_invariance',
+    version='0.0.1',
+    description='SE(3)-equivariant neural networks for hotspot prediction.',
+    author='Jack Scantlebury',
+    packages='tepid_invariance',
+    install_requires=[
+        'torch',
+        'numpy',
+        'pandas',
+        'wandb',
+        'yaml',
+        'pyyaml',
+        'einops',
+        'rdkit',
+        'matplotlib',
+        'openbabel',
+        'scipy'
+    ],
+)
